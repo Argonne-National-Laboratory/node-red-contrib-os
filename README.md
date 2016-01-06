2ccb6405541b8b4dae5c412fba1a8a8ffe761104
Jason D. Harper <jharper@anl.gov>
2016-01-06 15:31:13 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+node-red-contrib-os
+===========================
+
+[![NPM](https://nodei.co/npm/node-red-contrib-os.png)](https://nodei.co/npm/node-red-contrib-os/)
+
+[Node-Red][1] nodes for obtaining system information.  
+
+These nodes utilize the [Node.js OS Library][2] to obtain basic operating-system related utility functions.
+
+#Install
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-contrib-os
+
+#Author
+
+[Jason D. Harper][3]
+
+
+[1]:http://nodered.org
+[2]:https://nodejs.org/api/os.html
+[3]:https://github.com/jayharper
+
---
+++ os/os.js
@@ -0,0 +1,152 @@
+/**
+ * Copyright 2016 Argonne National Laboratory.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+    var RED = require(process.env.NODE_RED_HOME+"/red/red");
+    var settings = RED.settings;
+    var os = require('os');
+    var df = require('node-df');
+   
+    function OS(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };	
+		outmsg.payload = JSON.stringify({
+    					hostname: os.hostname(),
+    					type: os.type(),    					
+    					platform: os.platform(),
+    					arch: os.arch(),
+    					release: os.release(),
+    					endianness: os.endianness()    					
+  						});
+		node.send(outmsg);
+		}); 
+
+    }
+
+    RED.nodes.registerType("OS",OS);   
+
+	function Drives(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };	
+
+	    df(function (error, response) {
+    	if (error) { throw error; }
+    	outmsg.payload = JSON.stringify(response, null, 2);
+		node.send(outmsg);
+    	});
+		
+		}); 
+
+    }
+
+    RED.nodes.registerType("Drives",Drives);    
+
+	function Uptime(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };			
+		outmsg.payload = JSON.stringify({uptime: os.uptime()});
+		node.send(outmsg);
+		}); 
+    }
+
+    RED.nodes.registerType("Uptime",Uptime); 
+
+	function CPUs(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };			
+		outmsg.payload = JSON.stringify({cpus: os.cpus()});
+		node.send(outmsg);
+		}); 
+    }
+
+    RED.nodes.registerType("CPUs",CPUs);
+
+	function Loadavg(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };			
+		outmsg.payload = JSON.stringify({loadavg: os.loadavg()});
+		node.send(outmsg);
+		}); 
+    }
+
+    RED.nodes.registerType("Loadavg",Loadavg);
+
+	function Memory(n) {
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		var tmem = os.totalmem();
+		var fmem = os.freemem();
+		var pmem = ((fmem/tmem)*100).toFixed(2);
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };			
+		outmsg.payload = JSON.stringify({ totalmem: tmem, freemem: fmem, memusage: pmem});
+		node.send(outmsg);
+		}); 
+    }
+
+    RED.nodes.registerType("Memory",Memory);
+
+    function NetworkIntf(n) {        
+        RED.nodes.createNode(this,n);        
+		var node = this;
+		this.name = n.name;		 
+	
+        node.on("input", function(msg) {
+	    var outmsg = {
+	        topic: msg.topic
+	    };			
+		outmsg.payload = JSON.stringify({networkInterfaces: os.networkInterfaces()});
+		node.send(outmsg);
+		}); 
+    }
+
+    RED.nodes.registerType("NetworkIntf",NetworkIntf);   
+}
